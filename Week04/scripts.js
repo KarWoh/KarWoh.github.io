0eff72ba681e411c4dc3558a7c498dc515086886
Kar Woh <chongkarwoh96@gmail.com>
2020-12-04 20:38:59 +0800

    2039

--- Week04/scripts.js
+++ Week04/scripts.js
@@ -1,9 +1,21 @@
 function transformUpperCase(){
     let a = document.getElementById("namaPengguna");
-    a.value = a.value.transformUpperCase()
+    a.value = a.value.toUpperCase()
 
 }
 
 let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup",transformUpperCase);
+
+function checkPasswordLength(){
+    let password = document.getElementById("kataLaluan").value;
+    if(password.lenth <5){
+        alert("Password must be more 5 characters")
+    } else{
+        alert("Password accepted")
+    }
+}
+
+let elPassword = document.getElementById("KataLaluan");
+elPassword.onblur = checkPasswordLength;
 
