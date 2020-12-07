40fc762728dacd6a290d9c966ac936743732887e
Kar Woh <chongkarwoh96@gmail.com>
2020-12-07 22:03:16 +0800

    32423

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -25,7 +25,7 @@ function roundVal(val){
 }
 
 function calculateDisctances(lat, lon) {
-    let d1 = haversine(let, lon, 2.922562, 101.650965);// de pulze, cycberjaya
+    let d1 = haversine(lat, lon, 2.922562, 101.650965);// de pulze, cycberjaya
     let d2 = haversine(lat, lon, 3.073065, 101.607787);// Sunway Pyramid
     let d3 = haversine(lat, lon, 3.158761, 101.714524);// KLCC
 
