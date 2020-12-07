1a8dcb9f42f367f9dca9f4bcfadaeeb29b2784d3
Kar Woh <chongkarwoh96@gmail.com>
2020-12-07 22:12:03 +0800

    1233

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -43,7 +43,7 @@ elLocate.addEventListener("click", function(){
             let elKlcc = document.getElementById("klcc");
 
             let userLat = position.coords.latitude;
-            let userLong = position.coords.longtitude;
+            let userLong = position.coords.longitude;
 
             let distances = calculateDisctances(userLat, userLong);
             
