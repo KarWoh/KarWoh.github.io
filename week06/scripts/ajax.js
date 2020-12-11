f049354c29c62566b754f13658e096d44036cd55
Kar Woh <chongkarwoh96@gmail.com>
2020-12-11 20:56:37 +0800

    1234

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -4,24 +4,24 @@ function getRandomUser() {
     xhttp.onreadystatechange = function () {
         if(this.readyState == 4 && this.status == 200){
             let data = JSON.parse(this.response);
-            let elFirstName = document.getElementId("firstName");
-            let elLastName = document.getElementId("lastName");
-            let elJsonResult = document.getElementId("jsonResult");
-            let elUserImage = document.getElementId("userImage");
-            let elPhone = document.getElementId("phone");
-            let elAddress = document.getElementId("address");
+            let elFirstName = document.getElementById("firstName");
+            let elLastName = document.getElementById("lastName");
+            let elJsonResult = document.getElementById("jsonResult");
+            let elUserImage = document.getElementById("userImage");
+            let elPhone = document.getElementById("phone");
+            let elAddress = document.getElementById("address");
             let elEmail = document.getElementById("email");
 
             elJsonResult.innerHTML = this.response;
         }
     }
 
-    xhttp.open("GET", "https://randomuser.me/api/",true);
+    xhttp.open("GET", "https://randomuser.me/api/", true);
     xhttp.send();
 
 } 
 
-let elGetRandomUser = document.getElementId("getRandomUserBtn");
+let elGetRandomUser = document.getElementById("getRandomUserBtn");
 elGetRandomUser.addEventListener("click", function() {
     getRandomUser();
 });
