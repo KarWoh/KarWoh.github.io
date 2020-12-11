06f349917cebe8955d20b0cfe7e7659057466213
Kar Woh <chongkarwoh96@gmail.com>
2020-12-11 20:28:11 +0800

    2345

---
+++ week06/scripts/ajax.js
@@ -0,0 +1,22 @@
+function getRandomUser(){
+    let xhttp = new XMLHttpRequest(); //create request obj
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let data = JSON.parse(this.response);
+            let elFirstName = document.getElementId("firstName");
+            let elLastName = document.getElementId("lastName");
+            let elJsonResult = document.getElementId("jsonResult");
+            let elUserImage = document.getElementId("userImage");
+            let elPhone = document.getElementId("phone");
+            let elAddress = document.getElementId("address");
+            let elEmail = document.getElementById("email");
+
+            elJsonResult.innerHtml = this.response;
+        }
+    }
+
+    xhttp.open("GET", "https://randomuser.me/api/",true);
+    xhttp.send();
+
+}
