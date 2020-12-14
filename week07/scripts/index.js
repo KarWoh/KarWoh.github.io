0c2070094cd76c44613abe5ac4586704cfa6925f
Kar Woh <chongkarwoh96@gmail.com>
2020-12-14 21:11:40 +0800

    2345

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -7,7 +7,7 @@ let app = new Vue({
 
 let app2 = new Vue ({
     el:"#app2",
-    data:{
+    data: {
         message: "You loaded this page on" + new Date().toLocaleString(),
         linkURL: "https://bbc.in/2LAnJs8",
         linkText: "Google is down!"
