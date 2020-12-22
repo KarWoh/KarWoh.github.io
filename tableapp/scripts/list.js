4b2f70b2fb771a87b21650e792e5de51ab4d4423
Kar Woh <chongkarwoh96@gmail.com>
2020-12-22 20:57:24 +0800

    4325

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -22,8 +22,8 @@ function GetBookings(){
             row.insertCell(2).innerHTML = gEmail;
             row.insertCell(3).innerHTML = gPax;
             row.insertCell(4).innerHTML = gRemarks;
-            row.insertCell(5).innerHTML = "<buttong id='" + 
-                buttonId + "'class='btn btn-danger'>Delete</button>";
+            row.insertCell(5).innerHTML = 
+            "<buttong id='" + buttonId + "'class='btn btn-danger'>Delete</button>";
 
             bookingIds.push(buttonId);
         }
